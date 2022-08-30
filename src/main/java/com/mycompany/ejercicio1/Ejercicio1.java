6a0adb0da885579691adcb1ffb463e0f36713455
dav1jim <davidricardojimenezlopez@gmail.com>
2022-08-30 11:00:34 -0600

    Mi familia

---
+++ src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio1;
+
+/**
+ *
+ * @author david
+ */
+//Ponte creativo y preséntanos a tu familia con variables, utiliza todos los
+//tipos y cantidad de variables que puedas, no olvides las convenciones de
+//nombres Camel Case.
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+    
+        System.out.println("Ellos son mi famia");
+        System.out.println("Mi mama es: Yanira de Jimenez");
+        System.out.println("Mi papa es: Ramon Jimenez");
+        System.out.println("Mi hemana es: Nancy Jimeez");
+        System.out.println("Mi nombre es: David Jimenez");
+        
+    }
+}
